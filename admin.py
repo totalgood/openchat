e939f45d99d358ce9ba34b09a93a72c72826fe62
Hobson Lane <github@totalgood.com>
2017-01-08 15:12:39 -0800

    list display tweets

--- admin.py
+++ admin.py
@@ -8,6 +8,7 @@ admin.site.register(Place)
 
 class TweetAdmin(admin.ModelAdmin):
     date_hierarchy = 'created_date'
+    list_display = ('id_str', 'user__id' 'user__name', 'created_date', 'created_at', 'text')
 
 
 admin.site.register(Tweet, TweetAdmin)
