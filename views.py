02f9e26fd6bf7eec9b65e4c16a622813a0159760
Hobson Lane <github@totalgood.com>
2017-01-22 19:23:53 -0800

    inconsistent white space formatting in views.py

--- views.py
+++ views.py
@@ -17,33 +17,33 @@ def api_root(request, format=None):
 
 
 class HashtagList(generics.ListAPIView):
-	"""Matches any hashtag that is present in the tags field of a tweet.
+  """Matches any hashtag that is present in the tags field of a tweet.
 
-	   To format a request use a querystring as seen below:
+     To format a request use a querystring as seen below:
 
-	   Ex.
-	   twote/tags/?hashtag=TargetHashtagHere
-	   twote/tags/?hashtag=Frog --> will return all tweets with Frog as a hashtag
+     Ex.
+     twote/tags/?hashtag=TargetHashtagHere
+     twote/tags/?hashtag=Frog --> will return all tweets with Frog as a hashtag
 
-	   Passing in a request with no value attached to hashtag will return all tweets that have
-	   at least one hash tag present in their tags field.
+     Passing in a request with no value attached to hashtag will return all tweets that have
+     at least one hash tag present in their tags field.
 
-	   Ex.
-	   twote/tags/?hashtag --> will return all tweets with 1+ hashtags
+     Ex.
+     twote/tags/?hashtag --> will return all tweets with 1+ hashtags
 
-	   Currently the matching is case sensitive but can be changed as needed. You're also only able
+     Currently the matching is case sensitive but can be changed as needed. You're also only able
            to search for one hashtag at the moment, could work on adding multiple hashtags in the future.
-	"""
+  """
 
-	serializer_class = TweetSerializer
+  serializer_class = TweetSerializer
 
-	def get_queryset(self):
-		queryset = Tweet.objects.all()
-		hashtag = self.request.query_params.get('hashtag', None)
+  def get_queryset(self):
+    queryset = Tweet.objects.all()
+    hashtag = self.request.query_params.get('hashtag', None)
 
-		if hashtag is not None:
-			queryset = queryset.filter(tags__regex=r"\y{}\y".format(hashtag))
-		return queryset
+    if hashtag is not None:
+      queryset = queryset.filter(tags__regex=r"\y{}\y".format(hashtag))
+    return queryset
 
 
 class UserTweetsList(generics.ListAPIView):
