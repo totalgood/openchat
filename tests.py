1e0a93fbd726cca9c7133faac4c1271eac96893d
Zak-kent <zak.kent@gmail.com>
2017-01-23 17:17:24 -0800

    added fixtures to tests to load test data

--- tests.py
+++ tests.py
@@ -5,7 +5,7 @@ import json
 class StrictViewTest(TestCase):
     """simple tests for strict endpoint"""
 
-    # need to build fitures for test DB and load here
+    fixtures = ['twote/fixtures/twote_fixture.json']
 
     def setUp(self):
         self.c = Client()
@@ -21,8 +21,8 @@ class StrictViewTest(TestCase):
         response = self.c.get('/twote/strict/')
         self.assertEqual(response.status_code, 200)
 
-    #def test_each_tweet_only_has_one_hashtag(self):
-        #response = self.get_request_helper()
-        #hashtags = [tweet["tags"] for tweet in response]
+    def test_each_tweet_only_has_one_hashtag(self):
+        response = self.get_request_helper()
+        hashtags = [tweet["tags"] for tweet in response]
 
-        #self.assertEqual(len(response), len(hashtags))
+        self.assertEqual(len(response), len(hashtags))
