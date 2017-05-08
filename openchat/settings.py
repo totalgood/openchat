3960b8b7b034144b65c1a3f444050614f427f132
Hobson Lane <hobson@totalgood.com>
2017-05-07 22:31:06 -0700

    allowed_hosts to accomidate test domains and subdomains

--- openchat/settings.py
+++ openchat/settings.py
@@ -34,7 +34,7 @@ if not SECRET_KEY:
     os.environ["DJANGO_SECRET_KEY"] = random_str()
     SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
-ALLOWED_HOSTS = ['totalgood.org', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['openchat.totalgood.org', 'localhost', '127.0.0.1', 'openchat.totalgood.test', 'totalgood.org']
 
 # Application definition
 
