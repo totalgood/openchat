956d8071a18188ed02c9d2d1fd8a33df4b39edfe
Zachary Kent <zak.kent@gmail.com>
2017-05-12 10:10:33 -0700

    added tests for retweet logic

--- tests_streambot.py
+++ tests_streambot.py
@@ -70,27 +70,44 @@ class TestStreambotMethods(unittest.TestCase):
         self.assertEqual(create_event.call_count, 1)
         self.assertEqual(schedule_tweets.call_count, 1)
 
-    @mock.patch("openspaces.bot_utils.tweet_utils.schedule_tweets")
-    @mock.patch("openspaces.bot_utils.db_utils.create_event")
     @mock.patch("streambot.Streambot.send_slack_message")
-    @mock.patch("openspaces.bot_utils.db_utils.check_time_room_conflict")
-    @mock.patch("openspaces.bot_utils.time_utils.convert_to_utc")
     @mock.patch("streambot.Streambot.parse_time_room")
-    def test_retweet_logic_with_only_valid_room(self, t_r_parse, 
-                                                time_convert, conflict,
-                                                slack_message, create_event,
-                                                schedule_tweets):
+    def test_retweet_logic_with_only_valid_room(self, t_r_parse, slack_message):
 
-        # with only 1 room only a slack message should be sent
+        # with only 1 room a single slack message should be sent
         t_r_parse.return_value = {
             "date": [],
             "room": ["A123"]
         }
         
         self.S_bot.retweet_logic("fake tweet", 12345, "screen_name", 12345)
+        self.assertEqual(slack_message.call_count, 1)
 
-        self.assertEqual(time_convert.call_count, 0)
+    @mock.patch("streambot.Streambot.send_slack_message")
+    @mock.patch("streambot.Streambot.parse_time_room")
+    def test_retweet_logic_with_only_valid_time(self, t_r_parse, slack_message):
 
+        # with only 1 time a single slack message should be sent
+        t_r_parse.return_value = {
+            "date": [datetime.datetime.utcnow()],
+            "room": []
+        }
+        
+        self.S_bot.retweet_logic("fake tweet", 12345, "screen_name", 12345)
+        self.assertEqual(slack_message.call_count, 1)
+
+    @mock.patch("streambot.Streambot.send_slack_message")
+    @mock.patch("streambot.Streambot.parse_time_room")
+    def test_retweet_logic_with_multiple_valid_rooms_times(self, t_r_parse, slack_message):
+
+        # with mulitple times and rooms only a slack message should be sent
+        t_r_parse.return_value = {
+            "date": [datetime.datetime.utcnow(), datetime.datetime.utcnow()],
+            "room": ["A123", "B123"]
+        }
+        
+        self.S_bot.retweet_logic("fake tweet", 12345, "screen_name", 12345)
+        self.assertEqual(slack_message.call_count, 1)
 
 
 if __name__ == '__main__':
