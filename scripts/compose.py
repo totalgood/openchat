7f09db42c176324927fef29ef35be322dc182a89
Hobson Lane <hobson@users.noreply.github.com>
2018-04-18 12:33:14 -0700

    some json that workd on ECS

--- scripts/compose.py
+++ scripts/compose.py
@@ -1,7 +1,64 @@
 #!/usr/bin/env python
-""" Docker Compose utilities """
+""" Docker Compose utilities and AWS ECS deployment automation
+
+AWSServiceRoleForECS = \
+    {
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Effect": "Allow",
+                "Action": [
+                    "ec2:AttachNetworkInterface",
+                    "ec2:CreateNetworkInterface",
+                    "ec2:CreateNetworkInterfacePermission",
+                    "ec2:DeleteNetworkInterface",
+                    "ec2:DeleteNetworkInterfacePermission",
+                    "ec2:Describe*",
+                    "ec2:DetachNetworkInterface",
+                    "elasticloadbalancing:DeregisterInstancesFromLoadBalancer",
+                    "elasticloadbalancing:DeregisterTargets",
+                    "elasticloadbalancing:Describe*",
+                    "elasticloadbalancing:RegisterInstancesWithLoadBalancer",
+                    "elasticloadbalancing:RegisterTargets",
+                    "route53:ChangeResourceRecordSets",
+                    "route53:CreateHealthCheck",
+                    "route53:DeleteHealthCheck",
+                    "route53:Get*",
+                    "route53:List*",
+                    "route53:UpdateHealthCheck",
+                    "servicediscovery:DeregisterInstance",
+                    "servicediscovery:Get*",
+                    "servicediscovery:List*",
+                    "servicediscovery:RegisterInstance",
+                    "servicediscovery:UpdateInstanceCustomHealthStatus"
+                ],
+                "Resource": "*"
+            }
+        ]
+    }
+
+AmazonECSTaskExecutionRolePolicy = \
+    {
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Effect": "Allow",
+                "Action": [
+                    "ecr:GetAuthorizationToken",
+                    "ecr:BatchCheckLayerAvailability",
+                    "ecr:GetDownloadUrlForLayer",
+                    "ecr:BatchGetImage",
+                    "logs:CreateLogStream",
+                    "logs:PutLogEvents"
+                ],
+                "Resource": "*"
+            }
+        ]
+    }
+"""
 import json
 import os
+import sys
 import subprocess
 import tempfile
 
@@ -54,6 +111,15 @@ def register_ecs(ecs_client=boto3.client('ecs'), ecs_task_definition=json.load('
     )
 
 
+def create_cluster(ecs_client=boto3.client('ecs'), name='openchat'):
+    """ Create an ECS cluster, return the boto3 cluster client object
+
+    Positional parameters:
+      name (str): cluster name (there must not be existing clusters with the same name)
+    """
+    return ecs_client.create_cluster(clusterName=name)
+
+
 def main(compose_file_path='docker-compose.yml', ecs_file_name='ecs-task.json', family='', task_role_arn=''):
     ecs_task_definition = compose2ecs(compose_file_path)
     ecs_task_path = os.path.join(os.path.dirname(compose_file_path), ecs_file_name)
@@ -66,5 +132,7 @@ def main(compose_file_path='docker-compose.yml', ecs_file_name='ecs-task.json', 
 if __name__ == '__main__':
     ecs_client = boto3.client('ecs')
     """Client interface for ECS"""
-
-    compose_file_path = sys.argv[1]
+    compose_file_path = sys.argv[1] if len(sys.argv) > 1 else 'docker-compose.yml'
+    task_json = compose2ecs(compose_file_path)
+    print(register_ecs(ecs_client=ecs_client, ecs_task_definition=task_json))
+    boto3.client('ecs')
